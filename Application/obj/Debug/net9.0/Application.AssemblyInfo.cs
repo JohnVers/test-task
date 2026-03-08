860d548689cb7965b7ebc0961caa7681225451b5
sadam-agashirinov <sadam.agashirinov@gmail.com>
2026-03-08 09:44:54 +0300

    feat: Добавить docker-compose.yml

--- Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b403c4275a724ea47362cb0022b51f5f2490af20")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09f26946ea85886c1c066366e86f6b988e0693b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
