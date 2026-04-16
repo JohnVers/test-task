5d30791dcd9951219de103a11270605df7ca4d86
John Vershinin <johnvershinin@outlook.com>
2026-04-16 15:30:58 +0300

    updated

--- task/Program.cs
+++ task/Program.cs
@@ -1,7 +1,29 @@
-using task;
+using System.Reflection;
 
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
 
-var host = builder.Build();
-host.Run();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    options.IncludeXmlComments(xmlPath);
+});
+
+// builder.Services.AddHostedService<Worker>();
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        options.RoutePrefix = string.Empty; 
+    });
+}
+
+app.UseRouting();
+app.MapControllers(); // No authorization needed
+app.Run();
